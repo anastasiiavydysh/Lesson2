84d8b80bc68d4f2ccad3ea167e0f2db0c75fafd7
анастасія <@DESKTOP-BQG54B8>
2022-11-02 19:44:59 +0200

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,4 @@
+﻿double pi = 3.141592653d;
+decimal ejler = 2.7182818284590452m;
+Console.WriteLine("PI = "+pi+"\nEjler = "+ejler);
+Console.ReadKey();
---
+++ Task3/Program.cs
@@ -0,0 +1,7 @@
+﻿string str1 = "\nмой рядок 1";
+string str2 = "\tмой рядок 2";
+string str3 = "\aмой рядок 3";
+
+Console.WriteLine(str1);
+Console.WriteLine(str2);
+Console.WriteLine(str3);
